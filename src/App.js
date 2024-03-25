be9bdaead6240ec86b5575d691c879a8769cd98c
tarikua <tarikuamisganaw@gmail.com>
2024-03-25 10:47:32 +0300

    date-picker

--- src/App.js
+++ src/App.js
@@ -1,124 +1,79 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-
+import { EtCalendar } from 'et-calendar-react';
+// import {
+//     convertToGC,
+//     toEthiopianDateString,
+//     toEthiopianMonthString,
+//     toEthiopianDayString} from ' gc-to-ethiopian-calendar'
+    import { EthDateTime, limits,  } from 'ethiopian-calendar-date-converter'
+    // import {   getEthiopicDate,   etchangetolong,   etchangetofull,   etchangetomedium,   etchangetoshort, } from "./dateconverter.js"
+    // import EthioDate from 'eth-calendar';
 function App() {
-    const initialValues = {
-        username: "",
-        email: "",
-        password: "",
-        confirmPassword: "",
-    };
-    const [formValues, setFormValues] = useState(initialValues);
-    const [formErrors, setFormErrors] = useState({});
-    const [isSubmit, setIsSubmit] = useState(false);
+    
+    const [selectedDate, setSelectedDate] = useState(null);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormValues({ ...formValues, [name]: value });
-    };
+    
+    const [ethiopianDate, setEthiopianDate] = useState(null); // State to store the Ethiopian date
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setFormErrors(validate(formValues));
-        setIsSubmit(true);
-    };
+    
+  const handleDateChange = (newDate) => {
+    const fullDateString = newDate.$d; // Assuming formatObject is where the data resides
+    const year = newDate.$y;
+    const month = newDate.$M; // Months are zero-indexed in JS
+    const day = newDate.$D + 1;
 
-    useEffect(() => {
-        console.log(formErrors);
-        if (Object.keys(formErrors).length === 0 && isSubmit) {
-            console.log(formValues);
-        }
-    }, [formErrors, formValues, isSubmit]);
-    const validate = (values) => {
-        const errors = {};
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-        if (!values.username) {
-            errors.username = "Username is required!";
-        }
-        if (!values.email) {
-            errors.email = "Email is required!";
-        } else if (!regex.test(values.email)) {
-            errors.email = "This is not a valid email format!";
-        }
-        if (!values.password) {
-            errors.password = "Password is required";
-        } else if (values.password.length < 4) {
-            errors.password = "Password must be more than 4 characters";
-        } else if (values.password.length > 10) {
-            errors.password = "Password cannot exceed more than 10 characters";
-        }
-        if (values.password !== values.confirmPassword) {
-            errors.confirmPassword = "Those passwords didn’t match. Try again.";
-        }
-        return errors;
-    };
+    const dateObject = new Date(year, month, day);
+    setSelectedDate(newDate);
 
+    const convertedEthiopianDate = EthDateTime.fromEuropeanDate(dateObject);
+ 
+    const formattedEthiopianDate = convertedEthiopianDate.toString({
+     
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+      lang:"am",
+      era: "ethiopic-amete-alem",
+    }); // Update the Ethiopian date state
+    setEthiopianDate(formattedEthiopianDate);
+  };
+    
+
+    
     return (
         <>
             <div className="bgImg"></div>
             <div className="container">
-                {Object.keys(formErrors).length === 0 && isSubmit ? (
-                    <div className="ui message success">
-                        Signed in successfully
-                    </div>
-                ) : (
-                    console.log("Entered Details", formValues)
-                )}
+                
 
-                <form onSubmit={handleSubmit}>
-                    <h1>Sign Up</h1>
+                <form >
+                    <h1>Date picker</h1>
+                    
                     <div className="ui divider"></div>
                     <div className="ui form">
-                        <div className="field">
-                            <label>Username</label>
-                            <input
-                                type="text"
-                                name="username"
-                                placeholder="Choose a username"
-                                value={formValues.username}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <p>{formErrors.username}</p>
-                        <div className="field">
-                            <label>Email</label>
-                            <input
-                                type="text"
-                                name="email"
-                                placeholder="Email"
-                                value={formValues.email}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <p>{formErrors.email}</p>
-                        <div className="field">
-                            <label>Password</label>
-                            <input
-                                type="password"
-                                name="password"
-                                placeholder="Password"
-                                value={formValues.password}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <p>{formErrors.password}</p>
-                        <div className="field">
-                            <label>Confirm Password</label>
-                            <input
-                                type="password"
-                                name="confirmPassword"
-                                placeholder="Confirm password"
-                                value={formValues.confirmPassword}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <p>{formErrors.confirmPassword}</p>
-                        <button className="fluid ui button blue">Submit</button>
+                        
+                        <EtCalendar
+      value={selectedDate}
+      onChange={handleDateChange}
+      calendarType={true} 
+      lang="am" 
+      style={{
+        width: '100%', 
+        borderRadius: '10px',
+        backgroundColor: '#fff5e6', 
+        border: '1px solid #dbab5f', 
+        boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.1)', 
+       
+        color: '#4a4a4a' // Darker gray for text
+      }}
+    />                
+     {ethiopianDate && ( // Display only if Ethiopian date is available
+                            <p>Ethiopian Date: {ethiopianDate}</p>
+                        )}
                     </div>
                 </form>
-                <div className="text">
-                    Already have an account? <span>Login</span>
-                </div>
+               
             </div>{" "}
         </>
     );
